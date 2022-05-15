9471e090be8481cccbe987423f8c1cf920920628
Susan Minott <susan_minott@yahoo.com>
2022-05-14 23:16:09 -0400

    commit tree b6741b9e4bd9ddc463073b2eed64935f73247edc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+// 1ST CODE:
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string){
+    let answer = array.filter(el => el === string || el === string.toLowerCase())
+    return answer
+};
+
+//2ND CODE;
+
+function fuzzyMatch(array, string){
+    let firstLetters = array.filter(el => el.startsWith(string))
+    return firstLetters
+};
+
+//3RD CODE:
+
+function matchName(array, string){
+    let match = array.filter(el => el.name === string)
+    return match
+}
